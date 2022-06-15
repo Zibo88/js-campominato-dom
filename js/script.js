adb5bbd38fd17ffd6fe2f5b4c128c5da30cf8aa1
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-06-15 21:55:17 +0200

    generate le celle

--- js/script.js
+++ js/script.js
@@ -22,45 +22,56 @@
 const myBtn = document.getElementById('my-btn');
 // console.log(myBtn)
 
+//Richiamo elementi del DOM
+const grid = document.getElementById('main-grid');
+console.log('questo è grid', grid);
+
+ // creo una variabiale che definisca il numeroMax di celle divise per livello di difficoltà
+let gridClass;
+let rangeMax;
+let dificultLevel;
+
 // da quando il bottone viene cliccato parte il gioco
 myBtn.addEventListener('click', gameStart)
     // console.log(myBtn)
 
-   function gameStart(){
-    //Richiamo elementi del DOM
-    let grid = document.getElementById('#main-grid');
-    console.log('questo è grid', grid);
-    
+    grid.innerHTML = '';
+    grid.classList.remove(gridClass);
+
+
+
+function gameStart(){
 
 
     // Raccolta dati
     // Chiedo all'utente che livello di difficioltà vuole scegliere
-    let dificultLevel = document.querySelector('#difficulty').value;
+    dificultLevel = parseInt(document.getElementById('difficulty').value);
     console.log('difficult level = ',dificultLevel);
 
-    // creo una variabiale che definisca il numeroMax di celle divise per livello di difficoltà
-    let rangeMax;
-    let gridClass;
 
-    if(dificultLevel === 'easy'){
-        rangeMax = 100;
-        gridClass = 'easy';
-    } else if(dificultLevel === 'hard'){
-        rangeMax = 81;
-        gridClass = 'hard';
-    } else if(dificultLevel === 'crazy'){
-        rangeMax = 49;
-        gridClass = 'crazy';
+    switch(dificultLevel) {
+        case 1:
+            rangeMax = 100;
+            gridClass = 'easy';
+            break;
+        case 2:
+            rangeMax = 81;
+            gridClass = 'hard';
+            break;
+        default:
+            rangeMax = 49;
+            gridClass = 'crazy';
+            break;
     }
 
-    // console.log('range max', rangeMax)
-    // console.log('Questo è grid class', gridClass)
+    // console.log('questa è la classe da aggiungere', gridClass)
 
+    console.log('range max', rangeMax)
 
     // Genero le bombe
     // genero 16 numeri in base al numero di bombe da generare, valore minimo e valore massimo del livello di riferimento (rangeMax)
     // evoco la funzione e salvo in una variabile
-    let bombs = 16;
+    const bombs = 16;
     let theBombs = generateBombs( bombs, 1, rangeMax);
     console.log("numero di bombe in base all'array the bombs", theBombs);
 
@@ -69,56 +80,32 @@ myBtn.addEventListener('click', gameStart)
     // console.log(userAttempts)
       // definisco quando termina il gioco e se vince o se perde creando un ciclo while
     //creo un array che faccia da contenitore dei numeri dati dall'utente
-    let gamePlay = true;
 
     let arrayUserNumber = [];
 
-   
-    // creo un ciclo while che determini la fine del gioco
-    // while(gamePlay === true){
-    //     // richiedo il numero all'utente finchè..
-    //     let userNumber = parseInt (prompt('inserisci un numero tra 1 e ' + rangeMax));
-
-    //     // se il numero generato include il numero dell'utente
-    //     if(theBombs.includes(userNumber)){
-    //         // il gioco termina
-    //         gamePlay = false
-    //         alert('Hai perso')
-
-    //         // altrimenti
-    //     } else {
-    //         // se l'array vuoto non continiene usernumber 
-    //         if(!arrayUserNumber.includes(userNumber)){
-    //             // lo pushamo
-    //             arrayUserNumber.push(userNumber);
-    //             console.log(arrayUserNumber)
-    //         }
-
-    //         // definiamo la vittoria
-    //         if (arrayUserNumber.length === userAttempts){
-    //             alert('hai vinto')
-    //         }
-    //     }
-    //     console.log(arrayUserNumber) 
-    //}
-
-    let numberOK = alert('hai indovinato ' + arrayUserNumber.length + ' numeri') 
-
-    // FUNZIONI CHE MANIPOLNO IL DOM
-
     // Genero una griglia
-    function generateGrid(){
+
         // aggiungo una classe all'elemento richiamato dal DOM
         grid.classList.add(gridClass);
+        // Creo una nuova cella con un ciclo for
+        for(i = 1; i <= rangeMax; i++ ){
+            // creo l'elemento da appendere
+            let newSquare = document.createElement ('div');
+            newSquare.innerHTML = `<span>${i}</span>`;
+            newSquare.classList.add('square')
+            // quando l'utente clicca su newsquare
+            newSquare.addEventListener('click', clikToSquare)
+            // appendo l'elemento
+            grid.append(newSquare);
+
+
+        // console.log('questi sono i numeri', rangeMax[i])
+
+        }
+
         
-        // creo una nuova cella
-        // for(i = 1; i <= )
-        
-    }
 
 }
-
-
 
 
 
@@ -129,25 +116,25 @@ myBtn.addEventListener('click', gameStart)
     function generateBombs (numberOfBombs, numMin, rangeMax){
         // creo un array vuoto dove inserire i numeri che verrnno generati
         let numberGenerateArray = [];
-        
+
         // creo un ciclo while che vada avanti per tutta la lunghezza dell'array che deve essere inferiore al numero di bombe
         while(numberGenerateArray.length < numberOfBombs){
             // evoco la funzione random e la salvo in una variabile
             let randomNumber = getRndInteger(numMin, rangeMax);
             // console.log(randomNumber)
             // pusho solo i numeri che non sono gia presenti nell'array
-        if(!numberGenerateArray.includes(randomNumber)){
+            if(!numberGenerateArray.includes(randomNumber)){
                 numberGenerateArray.push(randomNumber);
-        }  
+            }
 
-        }  
+        }
         console.log(numberGenerateArray)
 
-        return numberGenerateArray  
+        return numberGenerateArray
 
         // creo una funzione che generi numeri random
         function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
+            return Math.floor(Math.random() * (max - min + 1) ) + min;
         }
     }
 
